7576364570b086242636af7b567824b10f9532a7
stillmattwest <stillmattwest@gmail.com>
2025-07-26 14:20:23 -0700

    Replace SVG logo with image asset and update navigation logo display

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,8 +5,9 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ url('/') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                    <a href="{{ url('/') }}" class="flex items-center space-x-0">
+                        <x-application-logo class="block h-12 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                        <span class="text-white font-bold text-xl">CMS</span>
                     </a>
                 </div>
             </div>
