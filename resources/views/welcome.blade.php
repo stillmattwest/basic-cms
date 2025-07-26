54d8905e0cd36a05ec9792e28e4ddb48b4c13e5c
stillmattwest <stillmattwest@gmail.com>
2025-07-26 14:00:07 -0700

    Initial Commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,18 @@
+<x-app-layout>
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="p-6 text-gray-900 dark:text-gray-100">
+                    <div class="text-center">
+                        <h1 class="text-4xl font-bold mb-4">
+                            Welcome to Basic CMS!
+                        </h1>
+                        <p class="text-lg text-gray-600 dark:text-gray-400">
+                            Your content management system is ready to go.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</x-app-layout>
