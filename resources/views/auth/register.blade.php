04bc7b1e8a20842f4a746e9149e4c1d5af416579
stillmattwest <stillmattwest@gmail.com>
2025-07-27 10:48:28 -0700

    Refactor guest layout components to include dynamic titles in login and registration views

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -1,4 +1,4 @@
-<x-guest-layout>
+<x-layouts.guest title="Register">
     <form method="POST" action="{{ route('register') }}">
         @csrf
 
@@ -49,4 +49,4 @@
             </x-primary-button>
         </div>
     </form>
-</x-guest-layout>
+</x-layouts.guest>
